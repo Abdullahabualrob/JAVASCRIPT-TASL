3e0fffcd950acb32fc121901b212090b2ac91a9c
RamiSerhan-0 <ramiserhan539@gmail.com>
2025-09-24 21:58:06 +0300

    Filtring added

--- Tasks.js
+++ Tasks.js
@@ -1,42 +1,52 @@
+///////////////////////////// page load /////////////////////////////////////
+let SavedTasks = JSON.parse(localStorage.getItem("Tasks")) || [];
 
-
+document.addEventListener("DOMContentLoaded", () => {
+  let Tasks = document.getElementById("Tasks");
+  console.log(SavedTasks);
+  SavedTasks.forEach((task) => {
+    Tasks.innerHTML += `
+    <div class="Task ${task.done ? "Done" : ""}">
+        <p>${task.name}</p>
+        <div class="Operations">
+          <input type="checkbox" ${task.done ? "checked" : ""}>
+          <button><i class="fa fa-pencil" aria-hidden="true"></i></button>
+          <button><i class="fa fa-trash" aria-hidden="true"></i></button>
+        </div>
+      </div>
+    `;
+  });
+});
 
 document.getElementById("addTaskBtn").addEventListener("click", () => {
-    /////////////////////// Validation ///////////////////////////////
-    
-    const input = document.getElementById("newToDo");
-    const taskName = document.getElementById("newToDo").value.trim();
-    let taskValidation = document.getElementById("taskValidation");
-    taskValidation.textContent = "";
-    taskValidation.style.display = "none";
-    if(taskName === "")
-    {
-        taskValidation.textContent = "Task name cannot be empty";
-        taskValidation.style.display = "block";
-        return;
-    }
-    else if (/^\d/.test(taskName))
-    {
-        taskValidation.textContent = "Task name cannot start with a number";
-        taskValidation.style.display = "block";
-        return;
-    }
-    else if (taskName.length < 5)
-    {
-        taskValidation.textContent = "Task name cannot be less than 5 characters";
-        taskValidation.style.display = "block";
-        return;
-    }
-    ////////////////////////////////////////////////////////////////
-    
+  /////////////////////// Validation ///////////////////////////////
 
-    ///////////////////// Addition ////////////////////////////////////////
-    let tasks = document.getElementById("Tasks");
-    SavedTasks.push(taskName);
-    // console.log(SavedTasks);
-    localStorage.setItem("Tasks", JSON.stringify(SavedTasks));
-    console.log(localStorage.getItem("Tasks"));
-    tasks.innerHTML += `
+  const input = document.getElementById("newToDo");
+  const taskName = document.getElementById("newToDo").value.trim();
+  let taskValidation = document.getElementById("taskValidation");
+  taskValidation.textContent = "";
+  taskValidation.style.display = "none";
+  if (taskName === "") {
+    taskValidation.textContent = "Task name cannot be empty";
+    taskValidation.style.display = "block";
+    return;
+  } else if (/^\d/.test(taskName)) {
+    taskValidation.textContent = "Task name cannot start with a number";
+    taskValidation.style.display = "block";
+    return;
+  } else if (taskName.length < 5) {
+    taskValidation.textContent = "Task name cannot be less than 5 characters";
+    taskValidation.style.display = "block";
+    return;
+  }
+
+
+  ///////////////////// Addition ////////////////////////////////////////
+  let tasks = document.getElementById("Tasks");
+  SavedTasks.push({name: taskName, done: false});
+  console.log(SavedTasks);
+  localStorage.setItem("Tasks", JSON.stringify(SavedTasks));
+  tasks.innerHTML += `
     <div class="Task">
         <p>${taskName}</p>
         <div class="Operations">
@@ -46,25 +56,60 @@ document.getElementById("addTaskBtn").addEventListener("click", () => {
         </div>
       </div>
     `;
-    input.value = "";
-  });
-///////////////////////////// page load /////////////////////////////////////
- let SavedTasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+  input.value = "";
+});
 
-  document.addEventListener("DOMContentLoaded", () => {
-    
-    let Tasks = document.getElementById("Tasks");
-    console.log(SavedTasks);
-    SavedTasks.forEach(task => {
-        Tasks.innerHTML += `
-    <div class="Task">
-        <p>${task}</p>
-        <div class="Operations">
-          <input type="checkbox">
-          <button><i class="fa fa-pencil" aria-hidden="true"></i></button>
-          <button><i class="fa fa-trash" aria-hidden="true"></i></button>
-        </div>
-      </div>
-    `;
-    });
+///////////////////////////// checkbox event /////////////////////////////////////
+document.getElementById("Tasks").addEventListener("change", (event) => {
+  if (event.target.type === "checkbox") {
+    const check = event.target;
+    const taskDiv = check.closest(".Task");
+    if (check.checked) {
+      taskDiv.classList.add("Done");
+    } else {
+      taskDiv.classList.remove("Done");
+    }
+    SavedTasks = SavedTasks.map((task) =>
+      task.name === taskDiv.querySelector("p").textContent
+        ? { ...task, done: check.checked }
+        : task
+    );
+    localStorage.setItem("Tasks", JSON.stringify(SavedTasks));
+    // console.log(SavedTasks);
+  }
+});
+///////////////////////////// Filtring /////////////////////////////////////
+//All
+document.getElementById("allTasks").addEventListener("click", () => {
+  const tasksDiv = document.getElementById("Tasks");
+  const tasks = tasksDiv.querySelectorAll(".Task");
+  tasks.forEach((task) => {
+    task.style.display = "flex";
+  });
+});
+//Done
+document.getElementById("doneTasks").addEventListener("click", () => {
+  const tasksDiv = document.getElementById("Tasks");
+  const tasks = tasksDiv.querySelectorAll(".Task");
+  tasks.forEach((task) => {
+    if (!task.classList.contains("Done")) {
+      task.style.display = "none";
+    }
+    else{
+      task.style.display = "flex";
+    }
+  });
+});
+//Todo
+document.getElementById("todoTasks").addEventListener("click", () => {
+  const tasksDiv = document.getElementById("Tasks");
+  const tasks = tasksDiv.querySelectorAll(".Task");
+  tasks.forEach((task) => {
+    if (task.classList.contains("Done")) {
+      task.style.display = "none";
+    }
+    else{
+      task.style.display = "flex";
+    }
+  });
 });
